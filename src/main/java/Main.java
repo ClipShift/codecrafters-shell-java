33eb77cd5fb87b0efb4ceccf15a5653c05ff12c3
Hitesh Kumar <hiteshkumar2499@gmail.com>
2024-12-03 23:06:52 +0530

    The type builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,25 +6,48 @@ public class Main {
         repl:
         while (true) {
             System.out.print("$ ");
-
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             String[] command = input.split(" ", 2);
-            switch (command[0]){
-                case "exit": {
-                    if("0".equals(command[1]))
-                        break repl;
-                }
+            if(isBuiltin(command[0])){
+                Builtin builtin = Builtin.valueOf(command[0]);
+                switch (builtin){
+                    case exit: {
+                        if("0".equals(command[1]))
+                            break repl;
+                    }
 
-                case "echo": {
-                    System.out.println(command[1]);
-                    break;
-                }
+                    case echo: {
+                        System.out.println(command[1]);
+                        break;
+                    }
 
-                default: {
-                    System.out.printf("%s: command not found%n", input);
+                    case type: {
+                        if(isBuiltin(command[1])){
+                            System.out.printf("%s is a shell builtin%n", command[1]);
+                        } else {
+                            System.out.printf("%s: command not found%n", command[1]);
+                        }
+                    }
                 }
+            } else {
+                System.out.printf("%s: command not found%n", input);
             }
         }
     }
+
+    private static boolean isBuiltin(String command){
+        try{
+            Builtin.valueOf(command);
+            return true;
+        } catch (IllegalArgumentException _){
+            return false;
+        }
+    }
+}
+
+enum Builtin {
+    exit,
+    echo,
+    type
 }
