f7899e88b1d21ffc2f08a7384218c40a60d11e5a
Hitesh Kumar <hiteshkumar2499@gmail.com>
2024-12-03 22:46:53 +0530

    The echo builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,15 +9,22 @@ public class Main {
 
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            String[] command = input.split(" ");
+            String[] command = input.split(" ", 2);
             switch (command[0]){
                 case "exit": {
                     if("0".equals(command[1]))
                         break repl;
                 }
-            }
 
-            System.out.printf("%s: command not found%n", input);
+                case "echo": {
+                    System.out.println(command[1]);
+                    break;
+                }
+
+                default: {
+                    System.out.printf("%s: command not found%n", input);
+                }
+            }
         }
     }
 }
