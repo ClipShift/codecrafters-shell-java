40d92802670d3aea996bb973fac194daafbb67b4
Hitesh Kumar <hiteshkumar2499@gmail.com>
2024-12-03 23:40:12 +0530

    Type builtin: executable files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,27 @@
-import java.util.Scanner;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        String pathEnv = System.getenv("PATH");
+        String[] paths = pathEnv.split(":");
+        Map<String, File> scripts = new HashMap<>();
+        for(String path: paths){
+            try (Stream<Path> directories = Files.walk(Paths.get(path))){
+                directories
+                        .filter(Files::isRegularFile)
+                        .filter(Files::isExecutable)
+                        .forEach(file -> scripts.put(String.valueOf(file.getFileName()), file.toFile()));
+            } catch (Exception _){
+
+            }
+        }
+
         // Uncomment this block to pass the first stage
         repl:
         while (true) {
@@ -25,7 +45,10 @@ public class Main {
                     case type: {
                         if(isBuiltin(command[1])){
                             System.out.printf("%s is a shell builtin%n", command[1]);
-                        } else {
+                        } else if (scripts.containsKey(command[1])){
+                            System.out.printf("%s is %s%n", command[1], scripts.get(command[1]).getPath());
+                        }
+                        else {
                             System.out.printf("%s: not found%n", command[1]);
                         }
                     }
